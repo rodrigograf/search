303dece9140786a838844263cbf9a3efa2b9bb03
Rodrigo Graf <rodrigo.graf@menlosecurity.com>
2017-12-28 10:24:14 -0800

    search

---
+++ js/search.js
@@ -0,0 +1,160 @@
+// JavaScript Document
+var BrowserDetect = {
+   init: function () {
+      this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+      this.version = this.searchVersion(navigator.userAgent) ||
+         this.searchVersion(navigator.appVersion) ||
+         "an unknown version";
+      this.OS = this.searchString(this.dataOS) || "an unknown OS";
+      this.bit = this.searchString(this.dataBit) || " x32";
+      this.OsVersion = this.searchString(this.dataOsVersion) || "an unknown OS version";
+   },
+   searchString: function (data) {
+      for (var i = 0; i < data.length; i++) {
+         var dataString = data[i].string;
+         var dataProp = data[i].prop;
+         this.versionSearchString = data[i].versionSearch || data[i].identity;
+         if (dataString) {
+            if (dataString.indexOf(data[i].subString) != -1)
+               return data[i].identity;
+         } else if (dataProp)
+            return data[i].identity;
+      }
+   },
+   searchVersion: function (dataString) {
+      var index = dataString.indexOf(this.versionSearchString);
+      if (index == -1) return;
+      return parseFloat(dataString.substring(index + this.versionSearchString.length + 1));
+   },
+   dataBrowser: [{
+      string: navigator.userAgent,
+      subString: "Chrome",
+      identity: "Chrome"
+   }, {
+      string: navigator.userAgent,
+      subString: "OmniWeb",
+      versionSearch: "OmniWeb/",
+      identity: "OmniWeb"
+   }, {
+      string: navigator.vendor,
+      subString: "Apple",
+      identity: "Safari",
+      versionSearch: "Version"
+   }, {
+      prop: window.opera,
+      identity: "Opera",
+      versionSearch: "Version"
+   }, {
+      string: navigator.vendor,
+      subString: "iCab",
+      identity: "iCab"
+   }, {
+      string: navigator.vendor,
+      subString: "KDE",
+      identity: "Konqueror"
+   }, {
+      string: navigator.userAgent,
+      subString: "Firefox",
+      identity: "Firefox"
+   }, {
+      string: navigator.vendor,
+      subString: "Camino",
+      identity: "Camino"
+   }, { // for newer Netscapes (6+)
+      string: navigator.userAgent,
+      subString: "Netscape",
+      identity: "Netscape"
+   }, {
+      string: navigator.userAgent,
+      subString: "MSIE",
+      identity: "Explorer",
+      versionSearch: "MSIE"
+   }, {
+      string: navigator.userAgent,
+      subString: "Gecko",
+      identity: "Mozilla",
+      versionSearch: "rv"
+   }, { // for older Netscapes (4-)
+      string: navigator.userAgent,
+      subString: "Mozilla",
+      identity: "Netscape",
+      versionSearch: "Mozilla"
+   }],
+   dataOS: [{
+      string: navigator.platform,
+      subString: "Win",
+      identity: "Windows"
+   }, {
+      string: navigator.platform,
+      subString: "Mac",
+      identity: "Mac"
+   }, {
+      string: navigator.userAgent,
+      subString: "iPhone",
+      identity: "iPhone/iPod"
+   }, {
+      string: navigator.platform,
+      subString: "Linux",
+      identity: "Linux"
+   }],
+   dataBit: [{
+      string: navigator.userAgent,
+      subString: "Win64",
+      identity: "x64"
+   }, {
+      string: navigator.userAgent,
+      subString: "WOW64",
+      identity: "x64"
+   }],
+   dataOsVersion: [{
+      string: navigator.userAgent,
+      subString: "NT 5.1",
+      identity: "XP"
+   }, {
+      string: navigator.userAgent,
+      subString: "NT 6.0",
+      identity: "Vista"
+   }, {
+      string: navigator.userAgent,
+      subString: "NT 6.1",
+      identity: "7"
+   }, {
+      string: navigator.userAgent,
+      subString: "NT 6.2",
+      identity: "8"
+   }]
+
+};
+
+BrowserDetect.init();
+
+if (BrowserDetect.browser === "Explorer") {
+   document
+      .getElementById("safeview-in-page-search-div")
+      .classList
+      .add("safeview-in-page-search-div-ie");
+}
+else if (BrowserDetect.OS === "Windows" && BrowserDetect.browser === "Mozilla") {
+   document
+      .getElementById("safeview-in-page-search-div")
+      .classList
+      .add("safeview-in-page-search-div-ie");
+}
+else if (BrowserDetect.browser === "Firefox") {
+   document
+      .getElementById("safeview-in-page-search-div")
+      .classList
+      .add("safeview-in-page-search-div-firefox");
+}
+else if (BrowserDetect.OS === "Windows" && BrowserDetect.browser === "Chrome") {
+   document
+      .getElementById("safeview-in-page-search-div")
+      .classList
+      .add("safeview-in-page-search-div-chrome-win");
+}
+else if (BrowserDetect.OS === "Windows" && BrowserDetect.browser === "Mozilla") {
+   document
+      .getElementById("safeview-in-page-search-div")
+      .classList
+      .add("safeview-in-page-search-div-chrome-win");
+}
